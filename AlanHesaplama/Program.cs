e1b4684e10da4d94cdc1777b981ef0d532a49abc
Zeynep İdil Gül <z_idilgul@hotmail.com>
2023-01-13 17:38:36 +0300

    Medium project 1: "Alan Hesaplama" done.

---
+++ AlanHesaplama/Program.cs
@@ -0,0 +1,97 @@
+using System;
+
+namespace AlanHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while (n != -1)
+            {
+                Console.WriteLine("1- Kare");
+                Console.WriteLine("2- Dikdörtgen");
+                Console.WriteLine("3- Üçgen");
+                Console.WriteLine("4- Daire");
+                Console.WriteLine("Çýkmak için -1 tuþlayýn");
+
+                Console.Write("Þekil numarasý giriniz: ");
+                bool isNum = int.TryParse(Console.ReadLine(), out n);
+                Methods inst = new Methods();
+
+
+                switch (n)
+                {
+                    case -1:
+                        Console.WriteLine("Program sona erdi");
+                        break;
+                    case 1:
+                        inst.CalculateAreaSquare();
+                        break;
+                    case 2:
+                        inst.CalculateAreaRectangle();
+                        break;
+                    case 3:
+                        inst.CalculateAreaTriangle();
+                        break;
+                    case 4:
+                        inst.CalculateAreaCircle();
+                        break;
+                    default:
+                        Console.WriteLine("Hatalý giriþ yaptýnýz");
+                        break;
+
+                }
+            }
+        }
+    }
+
+    public class Methods
+    {
+        public void CalculateAreaSquare()
+        {
+            Console.Write("Kenar uzunluðu giriniz: ");
+            int k = int.Parse(Console.ReadLine());
+
+            int area = k * k;
+
+            Console.WriteLine("Karenin alaný: " + area);
+        }
+
+        public void CalculateAreaRectangle()
+        {
+            Console.Write("1.kenar uzunluðu giriniz: ");
+            int k = int.Parse(Console.ReadLine());
+            Console.Write("2.kenar uzunluðu giriniz: ");
+            int m = int.Parse(Console.ReadLine());
+
+            int area = k * m;
+
+            Console.WriteLine("Karenin alaný: " + area);
+        }
+
+        public void CalculateAreaTriangle()
+        {
+            Console.Write("Kenar uzunluðu giriniz: ");
+            int k = int.Parse(Console.ReadLine());
+            Console.Write("Yükseklik giriniz: ");
+            int h = int.Parse(Console.ReadLine());
+
+            int area = (k * h) / 2;
+
+            Console.WriteLine("Karenin alaný: " + area);
+        }
+
+        public void CalculateAreaCircle()
+        {
+            Console.Write("Yarýçap uzunluðu giriniz: ");
+            int r = int.Parse(Console.ReadLine());
+            const double pi = 3.14;
+            double area = (pi * r * r);
+
+            Console.WriteLine("Karenin alaný: " + area);
+        }
+
+    }
+
+}
