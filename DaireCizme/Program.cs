be3d6be38c37882d427754211c8cea15f8e09513
Zeynep İdil Gül <z_idilgul@hotmail.com>
2023-01-13 17:40:59 +0300

    Easy project 3: "Daire Çizme" done.

---
+++ DaireCizme/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DaireCizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius of circle: ");
+            int r = int.Parse(Console.ReadLine());
+
+            Methods instance = new Methods();
+            instance.DrawCircle(r);
+
+        }
+    }
+
+    class Methods
+    {
+        public void DrawCircle(int yaricap)
+        {
+
+            for (double y = -yaricap; y <= yaricap; y++)
+            {
+                for (double x = -yaricap; x <= yaricap; x += 1)
+                {
+                    if ((x * x) + (y * y) >= yaricap * yaricap)
+                        Console.Write("  ");
+                    else
+                        Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
