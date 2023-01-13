d75da86e16f18cd958963a4c581c4ca6cbc867e4
Zeynep İdil Gül <z_idilgul@hotmail.com>
2023-01-13 17:42:01 +0300

    Easy project 5: "Karakter Tersten Yazdırma" done.

---
+++ KarakterTerstenYazdirma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace KarakterTerstenYazdirma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to shift: ");
+
+            string input = Console.ReadLine();
+
+            string[] strings = input.Split(" ");
+
+            Methods instance = new Methods();
+
+            foreach (var word in strings)
+            {
+                string s = instance.ShiftedString(word.ToString());
+                Console.WriteLine(s);
+            }
+        }
+    }
+
+    class Methods
+    {
+        public string ShiftedString(string t)
+        {
+            return t.Substring(1, t.Length - 1) + t.Substring(0, 1);
+        }
+    }
+}
