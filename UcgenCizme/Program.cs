f837d943fc00dd19c96bd7870ff7fbfee9da6245
Zeynep İdil Gül <z_idilgul@hotmail.com>
2023-01-13 17:33:40 +0300

    Easy project 2: "Üçgen Çizme" done.

---
+++ UcgenCizme/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UcgenCizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the row number of triangle: ");
+            int n = int.Parse(Console.ReadLine());
+            Methods instance = new Methods();
+            instance.Draw(n);
+        }
+
+    }
+    class Methods
+    {
+        public void Draw(int n)
+        {
+            for(int i = 0; i <= n; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
